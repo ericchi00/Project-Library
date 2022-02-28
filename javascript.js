7d01a634c841f0b63b66b69265e7482a7170f6a1
ericchi00 <ericchi00@gmail.com>
2022-02-27 17:53:47 -0800

    added logic to change read status

--- javascript.js
+++ javascript.js
@@ -2,7 +2,6 @@ let myLibrary = [];
 
 const contentWrapper = document.querySelector('.contentWrapper');
 
-
 //creates book object
 class Book {
     constructor(title, author, pages, read) {
@@ -10,6 +9,15 @@ class Book {
         this.author = author;
         this.pages = pages;
         this.read = read;
+    }
+}
+
+//changes read in obj
+Book.prototype.changeReadStatus = function () {
+    if (this.read === "No") {
+        this.read = "Yes";
+    } else {
+        this.read = "No";
     }
 }
 
@@ -34,11 +42,11 @@ function createCard() {
         removeButton = document.createElement('button'),
         changeRead = document.createElement('button'),
     ]
-   // card.setAttribute('data-cardCount', myLibrary.length - 1);
     removeButton.classList.add('removeButton');
     removeButton.textContent = 'Remove';
     changeRead.classList.add('changeRead');
     changeRead.textContent = 'Change Read Status';
+    read.classList.add('change');
     for (i = 0; i < cardInfo.length; i++) {
         card.appendChild(cardInfo[i]);
     }
@@ -48,15 +56,18 @@ function createCard() {
 //displays entire library
 function displayLibrary(library) {
     createCard();
-    addRemoveButtonFunction();
     const cards = document.querySelectorAll('.card');
+    const readText = document.querySelectorAll('.change');
     library.forEach(function (book, index) {
         title.textContent = 'Title: ' + `${book.title}`;
         author.textContent = 'Author: ' + `${book.author}`;
         pages.textContent = 'Pages: ' + `${book.pages}`;
         read.textContent = 'Read: ' + `${book.read}`;
         cards[index].setAttribute('data-cardCount', index); //resets all the data-cardCount when one is deleted 
+        readText[index].setAttribute('data-change', index); //adds attribute to grab later to change textContent
     })
+    addChangeReadStatus();
+    addRemoveButtonFunction();
 }
 
 //adds function to all removeButtons
@@ -80,27 +91,16 @@ function removeCard() {
     }
 }
 
-// for (i = 0; i < myLibrary.length; i++) {
 
-//     if (document.querySelector(`[data-cardCount="${i}"]`) !== myLibrary[i]) {
-//         myLibrary.splice(i, 1);
-//     }
 
-/*
-        const card = document.querySelector(`[data-cardCount="${index}"]`);
-        if (card === myLibrary[index]) {
-            console.log(index);
-            card.querySelector(`[data-cardCount="${index}"]`).remove();
-            //card.closest('div').remove();
-            myLibrary.splice(index, 1);
-        } else {
-            console.log(index);
-        }
-        //      const card = document.querySelector(`[data-cardCount="${index}"]`)
-        //    card.closest('div').remove();
+function addChangeReadStatus() {
+    const bookIndex = changeRead.closest('.card').getAttribute('data-cardCount');
+    const change = document.querySelectorAll('.change');
+    changeRead.addEventListener('click', () => {
+        myLibrary[bookIndex].changeReadStatus();
+        change[bookIndex].textContent = 'Read: ' + `${myLibrary[bookIndex].read}`;
     })
-    */
-
+}
 
 //adds form input to library
 const submit = document.querySelector('#submit');
